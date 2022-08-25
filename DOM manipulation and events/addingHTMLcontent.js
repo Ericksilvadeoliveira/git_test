cddbe864bdd67ae3ca7d9feb2aaa7a0ef096152a
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-08-25 07:48:27 -0300

    more DOM manipulation and events

--- DOM manipulation and events/addingHTMLcontent.js
+++ DOM manipulation and events/addingHTMLcontent.js
@@ -65,5 +65,15 @@ btn.addEventListener('click', () => {
     alert('Hello World');
 })
 
+btn.addEventListener('click', function(e) {
+    console.log(e);
+});
 
+btn.addEventListener('click', function(e) {
+    console.log(e.target);
+});
+
+btn.addEventListener('click', function(e) {
+    e.target.style.background = 'blue';
+});
 
